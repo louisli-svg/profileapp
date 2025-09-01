836e186dd6c483838ee7b6f30aa9284e8f9914b9
louis-svg <louis.li@jinix.io>
2025-09-01 23:07:19 +0800

    solve the deploy problem caused by vite.svg

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
 })
